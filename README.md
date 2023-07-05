e14208b5f26d503ba9f105442bdbf7a76fbe14e9
Frad LEE <fradser@gmail.com>
2023-07-05 15:19:13 +0800

    feat(app): add spline version

--- README.md
+++
@@ -1,3 +0,0 @@
-# visionos-school
-
-This project was bootstrapped with [`create-r3f-app`](https://github.com/utsuboco/create-r3f-app)
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/D4cqlgXe7zMwbVV7/scene.splinecode');
